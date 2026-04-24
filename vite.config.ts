430d5b8fb67b877a8f8112c929e5f9314b6a5b3b
Wes <weslley_wenny@hotmail.com>
2026-04-23 21:35:25 -0300

    feat: gh-pages config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import path from "path"
+// import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
 export default defineConfig({
   plugins: [react()],
+  base: '/nome-do-seu-repositorio/', // <--- Adicione esta linha
 })
